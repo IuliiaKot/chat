8b0a2b2c1f83a4d8f47900ea701fa715cacacb3b
IuliiaKot <kotlenko.julia@gmail.com>
2017-10-04 13:38:02 -0400

    add Message controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
   resources :users, only: [:new, :create]
-  resources :conversations
+  resources :conversations, only: [:create] do
+    resources :messages, only: [:create]
+  end
 end
