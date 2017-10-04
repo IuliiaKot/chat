37f3c7e92c6d185638937a8fb689f36145f3404c
IuliiaKot <kotlenko.julia@gmail.com>
2017-10-04 13:44:19 -0400

    add login

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,15 @@
+class SessionController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      @errors = ['Invalid']
+      render 'new'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
   resources :users, only: [:new, :create]
+  resources :session, only: [:new, :create]
   resources :conversations, only: [:create] do
     resources :messages, only: [:create]
   end
